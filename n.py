5a8e08494ae4cbfd18aa57c30fdc9ac15619fb6f
Igor <igorr994@yandex.ru>
2024-04-08 11:01:20 +1000

    Создал класс Human

---
+++ n.py
@@ -0,0 +1,7 @@
+
+class Human():
+    def __init__(self, health, mana, strength, dexterity):
+        self.health = health
+        self.mana = mana
+        self.strength = strength
+        self.dexterity = dexterity
